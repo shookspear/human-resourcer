6045bb2ca00a999c15310198a56fc4b52b76ff27
Echeta Okeke <echeta@rogers.com>
2022-05-21 21:12:06 -0400

    removed references to express js

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-const express = require('express');
+//const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
 const db = require('./db/connection');
-//const apiRoutes = require('./routes/apiRoutes');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// const PORT = process.env.PORT || 3001;
+//const app = express();
 
 // Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-// Use apiRoutes
-//app.use('/api', apiRoutes);
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
+// // Default response for any other request (Not Found)
+// app.use((req, res) => {
+//   res.status(404).end();
+// });
 
 // Start server after DB connection
 db.connect(err => {
   if (err) throw err;
   console.log('Database connected.');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
 });
